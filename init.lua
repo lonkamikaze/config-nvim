86e53fdc27ca03e37ca863ab8f78530e739c23d5
Dominic Fandrey <kamikaze@bsdforen.de>
2024-01-19 11:20:14 +0100

    My initial neovim config

---
+++ init.lua
@@ -0,0 +1,27 @@
+require 'paq' {
+	'savq/paq-nvim',
+	-- lualine
+	'nvim-tree/nvim-web-devicons',
+	'nvim-lualine/lualine.nvim',
+	-- treesitter
+	'nvim-treesitter/nvim-treesitter-textobjects',
+	'nvim-treesitter/nvim-treesitter',
+}
+vim.cmd('PaqSync')                    -- run on config change
+
+require('lualine').setup()            -- powerline alike
+                                      -- @see https://github.com/ryanoasis/nerd-fonts/releases
+require('nvim-treesitter').setup()    -- syntax highlighting, code completion
+
+-- colour scheme
+vim.g.codedark_transparent = 1        -- transparent background
+vim.cmd('colorscheme vim-code-dark')  -- activate
+
+-- override nvim defaults
+vim.o.termguicolors = true            -- use 24 bit colours
+vim.o.mouse = ''                      -- leave the mouse to the TE
+vim.cmd('nnoremap Y yy')              -- vim-like Y
+vim.o.hlsearch = true                 -- highlight search results
+vim.o.colorcolumn = '64,72,80,120'    -- highlight columns
+vim.o.listchars = 'tab:▷ ◁,trail:□'   -- display tabs and trailing space
+vim.o.list = true                     -- ... enable
